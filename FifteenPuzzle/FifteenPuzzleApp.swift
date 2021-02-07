f71d9d0dc1456ea22f9382215d9511a3fdb8c1e2
guygreenleaf <yaweh23@gmail.com>
2021-02-07 08:29:39 -0800

    Initial Commit

---
+++ FifteenPuzzle/FifteenPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifteenPuzzleApp.swift
+//  FifteenPuzzle
+//
+//  Created by Guy Greenleaf on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FifteenPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
